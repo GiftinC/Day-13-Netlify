f8996a5c88eafcb0fd94548e657b6309b06e8e61
GiftinC <cgiftin@gmail.com>
2024-05-19 19:21:08 +0530

    Added Day 13 Netlify

---
+++ script.js
@@ -0,0 +1,12 @@
+function foo(){
+var FirstName = document.getElementById("Name1").value;
+var MiddleName = document.getElementById("Name2").value;
+var LastName = document.getElementById("Name3").value;
+var email = document.getElementById("email").value;
+var Phone = document.getElementById("Phone").value;
+console.log(`First Name:${FirstName} 
+Middle Name:${MiddleName} 
+Last Name:${LastName} 
+Email:${email} 
+Phone Number:${Phone}`)
+}
